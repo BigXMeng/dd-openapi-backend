455a5553aa39a815635713cf8b02ab9ce582aff2
liuxianmeng <2751187368@qq.com>
2025-07-30 02:39:06 +0800

    【模块打包】模块打包最佳实践

--- dd-openapi-main/dd-openapi-main-web/src/main/java/com/dd/openapi/main/web/service/internal/UserInterfaceInfoService.java
+++ dd-openapi-main/dd-openapi-main-web/src/main/java/com/dd/openapi/main/web/service/internal/UserInterfaceInfoService.java
@@ -2,6 +2,7 @@ package com.dd.openapi.main.web.service.internal;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.dd.openapi.main.web.model.DO.UserInterfaceInfoDO;
+import com.dd.openapi.main.web.model.req.userinterface.EnableInvokeInterfaceReq;
 
 /**
  * <p>
@@ -13,4 +14,10 @@ import com.dd.openapi.main.web.model.DO.UserInterfaceInfoDO;
  */
 public interface UserInterfaceInfoService extends IService<UserInterfaceInfoDO> {
 
+    /**
+     * 用户开通API调用次数
+     *
+     * @param req
+     */
+    void enableInvoke(EnableInvokeInterfaceReq req);
 }
